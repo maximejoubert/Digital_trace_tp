6cdaca8ea5546ad90f7a528c717d90bef75838bb
max <maxime.joubet>
2022-12-06 15:27:42 +0100

    TP1 refonctionnel

--- first_micro/main.py
+++ first_micro/main.py
@@ -1,2 +1,20 @@
-def app(event):
-    return "Hello, world!"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+# https://lfco2p.deta.dev
+
+def hello_world():
+    prefix_google = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-250385898-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-250385898-2');
+</script>
+"""
+    return prefix_google + "Hello World"
