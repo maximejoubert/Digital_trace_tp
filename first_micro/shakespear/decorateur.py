79a319619c8a5dfa20d268c374629db6e4d5b9a2
max <maxime.joubet>
2023-01-07 18:28:21 +0100

    shakerspear

---
+++ first_micro/shakespear/decorateur.py
@@ -0,0 +1,10 @@
+import time
+
+def log_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        return end_time - start_time
+    return wrapper
+
